5a8cf30ed5bb44923dfa09a49ec7b256926ac364
gxt <gauravkthakur2002@gmail.com>
2025-09-24 23:24:42 +0530

    split the fetched text into chunks using RecursiveCharacterTextSplitter from langchain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
 export default async function HomePage() {
 
   try {
-    const result = await fetch('/scrimba-info.txt');
+    const result = await fetch('https://en.wikipedia.org/wiki/Thomas_M%C3%BCller');
     const text = await result.text();
-    console.log("Fetched text content (not displayed on the page):", text);
+
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize:500,
+      separators: ['\n\n', '\n', ' ', ''], // default setting
+      chunkOverlap:50
+    });
+
+    const output = await splitter.createDocuments([text]);
+    console.log(output);
   } catch (err) {
     console.log(err);
   }
