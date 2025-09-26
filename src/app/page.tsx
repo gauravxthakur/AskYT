1179f833ca6827c994a54cf00a297dbc91b3ed5f
gxt <gauravkthakur2002@gmail.com>
2025-09-26 08:37:52 +0530

    upload the chunks to Supabase table (vector store)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase';
+import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/huggingface_transformers";
+
 
 export default async function HomePage() {
-
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
   try {
-    const result = await fetch('https://en.wikipedia.org/wiki/Thomas_M%C3%BCller');
+    const result = await fetch(`${baseUrl}/scrimba-info.txt`);
     const text = await result.text();
 
     const splitter = new RecursiveCharacterTextSplitter({
@@ -14,6 +18,19 @@ export default async function HomePage() {
 
     const output = await splitter.createDocuments([text]);
     console.log(output);
+
+    const sbApiKey = process.env.SUPABASE_API_KEY;
+    const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT;
+    const llmApiKey = process.env.LLM_API_KEY;
+
+    const client = createClient( sbUrl, sbApiKey);
+
+    const embeddings = new HuggingFaceTransformersEmbeddings({ model: "Xenova/all-MiniLM-L6-v2"});
+
+    await SupabaseVectorStore.fromDocuments(output, embeddings, {client, tableName: 'documents'})
+
+    console.log("embeddings uploaded successfully to the Supabase table.")
+
   } catch (err) {
     console.log(err);
   }
